d38ca57edaa28ef6f32eeb9d82872adb12114cfa
Axl Wang <axlwang1976@gmail.com>
2019-07-27 11:46:00 +0800

    add cartItem component in cart

--- src/components/cartDropdown/CartDropdown.js
+++ src/components/cartDropdown/CartDropdown.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
+import CartItem from './CartItem';
 import styles from './CartDropdown.module.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className={styles.cartDropdown}>
-    <div className={styles.cartItems}></div>
+    <div className={styles.cartItems}>
+      {cartItems.map(cartItem => (
+        <CartItem {...cartItem} key={cartItem.id} />
+      ))}
+    </div>
     <Button
       variant="contained"
       color="primary"
@@ -16,4 +23,10 @@ const CartDropdown = () => (
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart }) => ({ cartItems: cart.cartItems });
+
+CartDropdown.propTypes = {
+  cartItems: PropTypes.array,
+};
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cartDropdown/CartItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './CartItem.module.scss';
+
+export default function CartItem({ name, price, imageUrl, qty }) {
+  return (
+    <div className={styles.cartItem}>
+      <img src={imageUrl} alt="name" />
+      <div className={styles.itemDetails}>
+        <span className={styles.name}>{name}</span>
+        <span className={styles.price}>
+          {qty} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+CartItem.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
+  imageUrl: PropTypes.string,
+  qty: PropTypes.number,
+};
