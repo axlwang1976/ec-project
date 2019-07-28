1e31183a91dcbd2528aa3324470d434a3ae2f1c2
Axl Wang <axlwang1976@gmail.com>
2019-07-28 10:39:30 +0800

    add redux-persist for localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 import rootReducer from './reducers/rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleWares = [logger];
-const store = createStore(
+
+export const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(...middleWares))
 );
 
-export default store;
+export const persistor = persistStore(store);
