0102e0cb7cd1e511081a18391f9a0d77da0a6f6c
Axl Wang <axlwang1976@gmail.com>
2019-08-04 20:23:53 +0800

    add lazyload and suspense to improve performance. add errorHandle component to catch error and display on screen

---
+++ src/components/error/ErrorHandle.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './ErrorHandle.module.scss';
+import errorImage from '../../assets/qIufhof.png';
+
+export default class ErrorHandle extends Component {
+  state = { hasErrored: false };
+
+  static getDerivedStateFromError(error) {
+    return { hasErrored: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(`${error}: ${info}`);
+  }
+
+  render() {
+    const { hasErrored } = this.state;
+    const { children } = this.props;
+
+    if (hasErrored) {
+      return (
+        <div className={styles.errorHandle}>
+          <div>
+            <img src={errorImage} alt="error" />
+          </div>
+          <div className={styles.errorText}>Something went wrong...</div>
+        </div>
+      );
+    }
+    return children;
+  }
+}
+
+ErrorHandle.propTypes = {
+  children: PropTypes.object,
+};
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import Homepage from '../pages/homepage/Homepage';
-import Shop from '../pages/shop/Shop';
-import SignIn from '../pages/signin/SignIn';
-import Cart from '../pages/cart/Cart';
-import Checkout from '../pages/checkout/Checkout';
-import Collection from '../pages/collection/Collection';
+import Spinner from '../components/spinner/Spinner';
+import ErrorHandle from '../components/error/ErrorHandle';
 import '../transitions/fade.css';
+
+const Homepage = lazy(() => import('../pages/homepage/Homepage'));
+const Shop = lazy(() => import('../pages/shop/Shop'));
+const SignIn = lazy(() => import('../pages/signin/SignIn'));
+const Cart = lazy(() => import('../pages/cart/Cart'));
+const Checkout = lazy(() => import('../pages/checkout/Checkout'));
+const Collection = lazy(() => import('../pages/collection/Collection'));
 
 export default function Router() {
   return (
@@ -16,16 +19,20 @@ export default function Router() {
         <TransitionGroup>
           <CSSTransition key={location.key} classNames="fade" timeout={500}>
             <Switch location={location}>
-              <Route exact path="/" render={() => <Homepage />} />
-              <Route exact path="/shop" render={() => <Shop />} />
-              <Route exact path="/signin" render={() => <SignIn />} />
-              <Route exact path="/cart" render={() => <Cart />} />
-              <Route exact path="/checkout" render={() => <Checkout />} />
-              <Route
-                exact
-                path="/shop/:title"
-                render={routeProps => <Collection {...routeProps} />}
-              />
+              <ErrorHandle>
+                <Suspense fallback={<Spinner />}>
+                  <Route exact path="/" render={() => <Homepage />} />
+                  <Route exact path="/shop" render={() => <Shop />} />
+                  <Route exact path="/signin" render={() => <SignIn />} />
+                  <Route exact path="/cart" render={() => <Cart />} />
+                  <Route exact path="/checkout" render={() => <Checkout />} />
+                  <Route
+                    exact
+                    path="/shop/:title"
+                    render={routeProps => <Collection {...routeProps} />}
+                  />
+                </Suspense>
+              </ErrorHandle>
             </Switch>
           </CSSTransition>
         </TransitionGroup>
